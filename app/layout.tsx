3c98b81570cc8dbce8e1c86ac28ed6da2f9156c2
amankhannpl <amankhannpl@gmail.com>
2026-07-08 13:42:20 +0300

    fix: remove non-existent globals.css import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css"; // If you don't have this file, you can safely remove this line
 
 const inter = Inter({ subsets: ["latin"] });
 
